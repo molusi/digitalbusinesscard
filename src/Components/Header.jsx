ac04e3cf3dc81be528bca9f8c3bd7c5e813160f4
Abigail <92498978+molusi@users.noreply.github.com>
2022-06-26 05:00:27 +0200

    Update Header.jsx

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -7,7 +7,7 @@ const Header=()=>{
         
             <div className='header' style={{border:'none',backgroundColor:'inherit'}}>
             
-                <img className='img-fluid'  src={"https://am3pap003files.storage.live.com/y4m_WGQWZwU27ImtHqb6rACMc245TX-c7Rf8C0JGRQNmkacMuZuwNsAziFnifgpCaiA-EFl0iPY4NslbyvlaC_LC8l0BVB7CKhBak4lKx4eqYIoHmMnBJ9OVFtup0qo16tLThdlMsePy43kwcGDwQx4Ci7aO5DaJMQS8wB_TzuTkCnFl3SqgadSixrta5n6E-zb?width=959&height=959&cropmode=none"} style={{margin:'0 auto',border:'none',width:'50%',height:'45%'}} alt='myimage'  />
+                <img className='img-fluid'  src={"https://am3pap003files.storage.live.com/y4mV7Y3kTfeEEly_EGBolkE-inhL9oLzSM5qMK_n6C8xTm2ckq20L213F89d5OZwcCzzDVkxqNZcFK2fPxi82CvALFBJgifpyOXk_8Y4bhbn4XHzXMb0SGFz9xjlhBNGGItmuLMZhb2XKW4PRTnlBsp9HhigWJJZZTH60F7jBZwBy94q3eLOVHhkGh1I3MzMfUJ?width=2420&height=2359&cropmode=none"} style={{margin:'0 auto',border:'none',width:'50%',height:'45%'}} alt='myimage'  />
                 
                 <h2 className="name">Abigail Molusi</h2>
                 
